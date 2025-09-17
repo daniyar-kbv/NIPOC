8b96f16ce11977f92b9f9ffb9ecc40af91b1bcd0
Daniyar <daniyar.kbv@gmail.com>
2025-09-17 17:08:26 +0500

    Initial Commit

---
+++ NIPOC/NIPOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  NIPOCApp.swift
+//  NIPOC
+//
+//  Created by Daniyar Kurmanbayev on 2025-09-17.
+//
+
+import SwiftUI
+
+@main
+struct NIPOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
